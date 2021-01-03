0700a7b0373a05ad730dc3af415b2b38ca5d4481
Mostafa <fm_benlagha@esi.dz>
2021-01-03 15:54:54 +0100

    authentication middleware

--- src/api/middlewars/authentication.ts
+++ src/api/middlewars/authentication.ts
@@ -1,4 +1,8 @@
+import { NextFunction, Request, Response } from 'express';
 import { body } from 'express-validator';
+import { handleHttpError } from '../../utils/HttpResponseHandler';
+import jwt from 'jsonwebtoken';
+import { tokenSecret } from '../../config/enviroment';
 
 export const registerValidation = [
   body('email').isEmail(),
@@ -13,6 +17,24 @@ export const registerValidation = [
 ];
 
 export const refreshTokenValidation = body('refreshToken').notEmpty();
+
+export function authenticateUser(
+  req: Request | any,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const token = req.headers.authorization?.split('')[1];
+    if (!token) return handleHttpError(new Error('non authorized'), res, 403);
+    jwt.verify(token, tokenSecret, (err: any, user: any) => {
+      if (err) return handleHttpError(new Error('expired token'), res, 401);
+      req.user = user;
+      return next();
+    });
+  } catch (error) {
+    handleHttpError(error, res, 500);
+  }
+}
 
 export const loginValidation = [
   body('email').isEmail(),
